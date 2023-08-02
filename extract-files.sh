4f0cab9e7a2a98a8981b362e2f03ef994f0abb57
ThatMalayaliDeb <theoverpoweredguy@gmail.com>
2023-08-02 15:56:11 +0530

    Revert "sm8350-common: turn camera postproc's thread pool shrink into nop"
    This reverts commit 5f8156b0192f8d9e048a6ada803c3555faee345c.

--- extract-files.sh
+++ extract-files.sh
@@ -99,10 +99,6 @@ function blob_fixup() {
             hexdump -ve '1/1 "%.2X"' "${2}" | sed "s/7C000094881640F9/1F2003D5881640F9/g" | xxd -r -p > "${TMPDIR}/${1##*/}"
             mv "${TMPDIR}/${1##*/}" "${2}"
             ;;
-        vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
-            hexdump -ve '1/1 "%.2X"' "${2}" | sed "s/8D0A0094AE1640F9/1F2003D5AE1640F9/g" | xxd -r -p > "${TMPDIR}/${1##*/}"
-            mv "${TMPDIR}/${1##*/}" "${2}"
-            ;;
         vendor/lib64/hw/camera.xiaomi.so)
             "${SIGSCAN}" -p "4d 07 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
